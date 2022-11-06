53721b67f80962fa49b379ca0f9ea24614b46cb7
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-11-06 23:13:58 +0530

    feat: view permissions

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -4,13 +4,7 @@ import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
   const examples = await prisma.example.findMany();
-  console.log(
-    await prisma.user.findFirst({
-      where: {
-        email: "8.aniket.chowdhury@gmail.com",
-      },
-    })
-  );
+
   res.status(200).json(examples);
 };
 
---
+++ src/pages/api/initialize.ts
@@ -0,0 +1,75 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+import { getServerAuthSession } from "../../server/common/get-server-auth-session";
+import { prisma } from "../../server/db/client";
+
+enum Permission {
+  Read = "read",
+  Comment = "comment",
+  Like = "like",
+  Write = "write",
+}
+enum ExecutionLevel {
+  Public = "public",
+  LoggedIn = "logged-in",
+  Author = "author",
+  Admin = "admin",
+}
+
+const adminEmails = [
+  "8.aniket.chowdhury@gmail.com",
+  "abhinavrobinson@gmail.com",
+];
+
+const restricted = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerAuthSession({ req, res });
+
+  if (session) {
+    if (adminEmails.includes(session?.user?.email!)) {
+      const permissions = [
+        {
+          name: Permission.Read,
+          executionLevel: ExecutionLevel.Public,
+        },
+        {
+          name: Permission.Comment,
+          executionLevel: ExecutionLevel.LoggedIn,
+        },
+        {
+          name: Permission.Like,
+          executionLevel: ExecutionLevel.LoggedIn,
+        },
+        {
+          name: Permission.Write,
+          executionLevel: ExecutionLevel.Author,
+        },
+      ];
+
+      await Promise.all(
+        permissions.map(async (permission) => {
+          const perm = await prisma.permissions.findFirst({
+            where: {
+              name: permission.name,
+            },
+          });
+          if (!perm) {
+            await prisma.permissions.create({
+              data: permission,
+            });
+          }
+        })
+      );
+    }
+
+    res.send({
+      content: await prisma.permissions.findMany(),
+    });
+  } else {
+    res.send({
+      error:
+        "You must be signed in to view the protected content on this page.",
+    });
+  }
+};
+
+export default restricted;
