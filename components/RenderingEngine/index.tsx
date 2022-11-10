fcb65ae0603a15cead99ec1f8d7068b40af42cdf
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-11-10 23:19:00 +0530

    feat: added blogs

--- components/RenderingEngine/index.tsx
+++ components/RenderingEngine/index.tsx
@@ -2,23 +2,9 @@ import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
 
-const markdown = `
-# Some Blog Header
-
-### Data Header
-
-- data: 2019-01-01
-- data: 2019-02-01
-
-<a href="#">GoTo</a>
-
-\`\`\`js
-const a = 1;
-\`\`\`
-
-`;
-
-export const RenderingEngine: React.FC = () => {
+export const RenderingEngine: React.FC<{ markdown: string }> = ({
+  markdown,
+}) => {
   return (
     <>
       <div className="prose prose-a:text-blue-600 prose-img:rounded-xl">
--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,10 +1,36 @@
+import { readFileSync } from "fs";
+import { InferGetServerSidePropsType } from "next";
 import React from "react";
 import { RenderingEngine } from "../../../components/RenderingEngine";
 
-const Blog: React.FC = () => {
+// get server side props
+export const getServerSideProps = async (context: any) => {
+  const { id } = context.params;
+  const res = await fetch(new URL(`/blogs/${id}.md`, process.env.NEXTAUTH_URL));
+
+  return {
+    props: {
+      data: res.status === 200 ? await res.text() : "",
+      status: res.status === 200,
+    },
+  };
+};
+
+const Blog: React.FC<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ data, status }) => {
   return (
     <>
-      <RenderingEngine />
+      {status && (
+        <div className="grid place-content-center pt-10">
+          <RenderingEngine markdown={data} />
+        </div>
+      )}
+      {!status && (
+        <div className="prose">
+          <h1>404</h1>
+        </div>
+      )}
     </>
   );
 };
