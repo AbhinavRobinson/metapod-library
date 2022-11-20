c9b509f101f796818fff13f21e54182c3b1653e3
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-11-20 18:57:35 +0530

    feat: only for writers

--- src/pages/blog/create.tsx
+++ src/pages/blog/create.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useState } from "react";
 import { RenderingEngine } from "../../../components/RenderingEngine";
+import { trpc } from "../../utils/trpc";
 
 export const getServerSideProps: GetServerSideProps<
   { data: string; status: boolean },
@@ -25,36 +26,41 @@ const Create: React.FC<
     markdown: data,
   });
 
-  return (
-    <>
-      Create a blog
-      <div>
-        <input
-          className="border-black"
-          type="text"
-          name="title"
-          onChange={(e) => {
-            setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-          }}
-          placeholder="Title"
-        />
-      </div>
-      <div className="flex">
-        <textarea
-          className="w-1/2 rounded-md border p-2"
-          value={markdown}
-          name="markdown"
-          onChange={(e) => {
-            setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-          }}
-        />
-        <RenderingEngine markdown={markdown} />
-      </div>
-      <button className="rounded-md border px-3 py-2 text-xl text-black">
-        Create a Blog
-      </button>
-    </>
-  );
+  const { status, data: isWriter } = trpc.user.isWriter.useQuery();
+
+  if (status === "success" && isWriter) {
+    return (
+      <>
+        Create a blog
+        <div>
+          <input
+            className="border-black"
+            type="text"
+            name="title"
+            onChange={(e) => {
+              setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+            }}
+            placeholder="Title"
+          />
+        </div>
+        <div className="flex">
+          <textarea
+            className="w-1/2 rounded-md border p-2"
+            value={markdown}
+            name="markdown"
+            onChange={(e) => {
+              setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+            }}
+          />
+          <RenderingEngine markdown={markdown} />
+        </div>
+        <button className="rounded-md border px-3 py-2 text-xl text-black">
+          Create a Blog
+        </button>
+      </>
+    );
+  }
+  return <>404</>;
 };
 
 export default Create;
